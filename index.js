4e9270fe5a2c5095b71f76c132d2ada646816363
vijayreddynallamilli014-dotcom <vijayreddynallamilli014@gmail.com>
2025-12-27 22:03:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const furniture_items = [
+  {
+    name: 'Wooden Stool',
+    image_path: './Images/Deals-On-Furniture-1.png',
+    price: '10,000',
+  },
+  {
+    name: 'Office Chair',
+    image_path: './Images/Deals-On-Furniture-2.png',
+    price: '15,000',
+  },
+  {
+    name: 'Dining Table',
+    image_path: './Images/Deals-On-Furniture-3.png',
+    price: '25,000',
+  },
+  {
+    name: 'Sofa Set',
+    image_path: './Images/Deals-On-Furniture-4.png',
+    price: '40,000',
+  },
+];
